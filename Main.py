1a0363d2588d84175be85134e433ca3674fa3456
KIMYO9 <fgh33678@gmail.com>
2022-07-12 22:20:56 +0900

    first_commit - 2022-07-12 draw 회줜 성공

---
+++ Main.py
@@ -0,0 +1,99 @@
+import sys
+import pygame
+import math
+from pygame.locals import *
+
+
+pygame.init()
+
+FPS = 100
+FramePerSec = pygame.time.Clock()
+
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+SKYBLUE = (184,233,248)
+
+size = (600,600)
+GameDisplay = pygame.display.set_mode(size)
+pygame.display.set_caption("정사영 시뮬레이션")
+GameDisplay.fill(WHITE)
+
+font = pygame.font.SysFont('malgungothicsemilight', 20)
+
+
+seta = 0
+def stimulationStart():
+    global seta
+    draw()
+    while True:
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_LEFT:
+                    seta -= 0.1
+                    GameDisplay.fill(WHITE)
+                    draw()
+                elif event.key == K_RIGHT:
+                    seta += 0.1
+                    GameDisplay.fill(WHITE)
+                    draw()
+        
+        
+        # FramePerSec(FPS)
+
+
+def draw():
+    global seta
+    focus = (300, 500)
+    circle_r = 150
+
+    sin = math.sin(seta)
+    cos = math.cos(seta)
+    down_right_point = (300+int(cos*circle_r), 500+int(sin*circle_r))
+    down_left_point = (300-int(cos*circle_r), 500-int(sin*circle_r))
+
+    pygame.draw.line(GameDisplay, BLACK, down_left_point, down_right_point)
+
+# def draw():
+#     focus = (300, 500)
+#     circle_r = 150
+
+#     sin = math.sin(seta)
+#     cos = math.cos(seta)
+#     down_right_point = (300+int(cos*circle_r), 500+int(sin*circle_r))
+#     down_left_point = (300-int(cos*circle_r), 500-int(sin*circle_r))
+
+#     # def quadratic_formula(a, b, c):
+#     #     '''
+#     #     근의 공식
+#     #     '''
+#     #     return ((-b+math.sqrt(b**2-4*a*c))/2*a, (-b-math.sqrt(b**2-4*a*c))/2*a)
+
+    
+#     # try:
+#     #     a = (cos*circle_r)**2/(sin*circle_r)**2+1
+#     # except:
+#     #     a = 0
+#     # try:
+#     #     b = 2*(circle_r**2)*(cos*circle_r)/(sin*circle_r)**2
+#     # except:
+#     #     b = 0
+#     # try:
+#     #     c = circle_r**4/(sin*circle_r)**2 - 2*circle_r**2 + (cos*circle_r)**2
+#     # except:
+#     #     c = 0
+#     # top_left_x = int(quadratic_formula(a, b, c)[0])+300
+#     # try:
+#     #     top_left_y = int(circle_r**2-top_left_x*cos/sin)+500
+#     # except:
+#     #     top_left_y = 0
+#     # top_left_point = (top_left_x, top_left_y)
+
+#     pygame.draw.line(GameDisplay, BLACK, down_left_point, down_right_point)
+#     # pygame.draw.line(GameDisplay, BLACK, down_left_point, top_left_point)
+
+
+stimulationStart()
