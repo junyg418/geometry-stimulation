726daa21004e693ce7a7fb770cde0135ca207aba
KIMYO9 <fgh33678@gmail.com>
2022-07-13 15:16:19 +0900

    완성본

--- Main.py
+++ Main.py
@@ -12,6 +12,7 @@ FramePerSec = pygame.time.Clock()
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 SKYBLUE = (184,233,248)
+YELLO = (255,212,0)
 
 size = (600,600)
 GameDisplay = pygame.display.set_mode(size)
@@ -19,6 +20,7 @@ pygame.display.set_caption("정사영 시뮬레이션")
 GameDisplay.fill(WHITE)
 
 font = pygame.font.SysFont('malgungothicsemilight', 20)
+text = font.render('그림자', True, BLACK)
 
 
 seta = 0
@@ -47,16 +49,29 @@ def stimulationStart():
 
 def draw():
     global seta
-    focus = (300, 500)
+    focus = (300, 300)
     circle_r = 150
 
     sin = math.sin(seta)
     cos = math.cos(seta)
-    down_right_point = (300+int(cos*circle_r), 500+int(sin*circle_r))
-    down_left_point = (300-int(cos*circle_r), 500-int(sin*circle_r))
+    up_right_point = (300+int(cos*circle_r), 300+int(sin*circle_r))
+    up_left_point = (300-int(cos*circle_r), 300-int(sin*circle_r))
 
-    pygame.draw.line(GameDisplay, BLACK, down_left_point, down_right_point)
+    pygame.draw.line(GameDisplay, BLACK, up_left_point, up_right_point, 3)
 
+
+    # 정사영 그림자 코드-----
+
+    down_right_x = int(150*cos)+300
+    down_right_point = (down_right_x, 500)
+    
+    down_left_x = 300-int(150*cos)
+    down_left_point = (down_left_x, 500)
+
+    pygame.draw.line(GameDisplay, BLACK, down_right_point, down_left_point, 3)
+
+    pygame.draw.circle(GameDisplay, YELLO, (300,50), 30)
+    GameDisplay.blit(text, (20, 485))
 # def draw():
 #     focus = (300, 500)
 #     circle_r = 150
